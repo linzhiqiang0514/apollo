4e9c09db176f596958c3e074084fbd46d63bda26
林志强 <linzhiqiang0514@163.com>
2019-07-29 08:42:32 +0800

    调整注解。

--- apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/ConfigServerEurekaServerConfigure.java
+++ apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/ConfigServerEurekaServerConfigure.java
@@ -5,14 +5,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * .
+ * Start Eureka Server annotations according to configuration
  *
- * @author linzhiqiang
- * @version Revision 1.0.0
- * @版权： 版权所有 (c) 2011
- * @see:
- * @创建日期： 2019-07-25
- * @功能说明：
+ * @author Zhiqiang Lin(linzhiqiang0514@163.com)
  */
 @Configuration
 @EnableEurekaServer
